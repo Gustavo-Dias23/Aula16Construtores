531df9efce519e88d2b57c88042ab6ff9bfbafdb
Gustavo Dias <dias.gustavo23@gmail.com>
2020-06-12 09:20:36 -0300

    Inicio

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula16Construstores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto sapato = new Produto();
+
+            Produto chinelo = new Produto(987);
+            System.Console.WriteLine(chinelo.Codigo);
+        }
+    }
+}
