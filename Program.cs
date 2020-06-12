683e171633bdc206bfccf4a05364acd147609e04
Gustavo Dias <dias.gustavo23@gmail.com>
2020-06-12 09:31:26 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ namespace Aula16Construstores
     {
         static void Main(string[] args)
         {
-            Produto sapato = new Produto();
+            Produto carro = new Produto();
 
-            Produto chinelo = new Produto(987);
-            System.Console.WriteLine(chinelo.Codigo);
+            Produto moto = new Produto(987);
+            System.Console.WriteLine(moto.Codigo);
+
+            Produto van = new Produto(321,"Kombi", "Azul", 3);
+            System.Console.WriteLine("O código do produto é "+van.Codigo);
+            System.Console.WriteLine("O modelo do produto é "+van.Nome);
+            System.Console.WriteLine("A cor do produto é "+van.Descricao);
+            System.Console.WriteLine("O número desse tipo de produto no estoque é "+van.Estoque);
         }
     }
 }
